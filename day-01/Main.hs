bc1d66b2148143b279ea72e39e46646b912e442d
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-01 18:17:36 +0000

    do day 1, part 1

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -1,2 +1,33 @@
+import Control.Monad (guard)
+import Data.ByteString qualified as B
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as TE
+import Data.Text.Read qualified as TR
+import Flow ((.>))
+
+
 main :: IO ()
-main = pure ()
+main = do
+    input <- parseInput <$> readFileUtf8 "input.txt"
+    print $ part1 input
+
+part1 :: [Int] -> Int
+part1 = findPairSumsTo 2020 .> uncurry (*)
+
+findPairSumsTo :: (Eq a, Num a) => a -> [a] -> (a, a)
+findPairSumsTo target list = head $ do
+    x <- list
+    y <- list
+    guard $ x + y == target
+    pure (x, y)
+
+readFileUtf8 :: FilePath -> IO T.Text
+readFileUtf8 path = TE.decodeUtf8 <$> B.readFile path
+
+parseInput :: T.Text -> [Int]
+parseInput = T.lines .> fmap readInt
+  where
+    readInt :: T.Text -> Int
+    readInt txt = case TR.decimal txt of
+        Left _ -> error "parseInput: input not an Int"
+        Right (n, _) -> n
