4d946b130dcd27ed9fdb24ffbeebdff5e0a135a9
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-04 15:57:09 +0000

    finish day 4, part 1
    We now assume zero structure about height and hair-colour fields.
    Despise the examples given in the problem description, the input
    doesn't follow the same structure for either field. As it's not needed
    currently, let's just not even bother trying to parse the contents of
    these fields.

--- day-04/Day04.hs
+++ day-04/Day04.hs
@@ -1,16 +1,12 @@
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Day04 where
 
-import Control.Applicative (Const, (<|>))
-import Control.Monad (replicateM, void)
+import Control.Applicative (Const)
 import Data.Bifunctor (second)
 import Data.ByteString qualified as B
-import Data.Functor (($>))
 import Data.Functor.Identity (Identity)
 import Data.Map qualified as M
 import Data.Maybe (mapMaybe)
@@ -32,8 +28,8 @@ data Creds f = MkCreds
     { birthYear :: Int
     , issueYear :: Int
     , expirationYear :: Int
-    , height :: (Int, Unit)
-    , hairColour :: RGB
+    , height :: T.Text
+    , hairColour :: T.Text
     , eyeColour :: T.Text
     , passportID :: T.Text
     , countryID :: f T.Text
@@ -43,18 +39,6 @@ deriving instance Eq (f T.Text) => Eq (Creds f)
 deriving instance Ord (f T.Text) => Ord (Creds f)
 deriving instance Show (f T.Text) => Show (Creds f)
 deriving instance Read (f T.Text) => Read (Creds f)
-
-data Unit = Cm | In
-    deriving (Bounded, Enum, Eq, Ord, Read, Show)
-
-data RGB = MkRGB
-    { red :: Int
-    , green :: Int
-    , blue :: Int
-    } deriving (Bounded, Eq, Ord, Read, Show)
-
-data V3 a = V3 !a !a !a
-    deriving (Bounded, Eq, Functor, Foldable, Ord, Read, Show)
 
 main :: IO ()
 main = do
@@ -69,8 +53,8 @@ parseCreds dict = do
     byr <- P.parseMaybe parseYear =<< M.lookup "byr" dict
     iyr <- P.parseMaybe parseYear =<< M.lookup "iyr" dict
     eyr <- P.parseMaybe parseYear =<< M.lookup "eyr" dict
-    hgt <- P.parseMaybe parseHeight =<< M.lookup "hgt" dict
-    hcl <- P.parseMaybe parseColour =<< M.lookup "hcl" dict
+    hgt <- M.lookup "hgt" dict
+    hcl <- M.lookup "hcl" dict
     ecl <- M.lookup "ecl" dict
     pid <- M.lookup "pid" dict
     let cid = M.lookup "cid" dict
@@ -87,21 +71,6 @@ parseCreds dict = do
 
 parseYear :: Parser Int
 parseYear = read <$> P.some PC.digitChar
-
-parseHeight :: Parser (Int, Unit)
-parseHeight = do
-    hgt <- read <$> P.some PC.digitChar
-    unit <- (PC.string "cm" $> Cm) <|> (PC.string "in" $> In)
-    pure (hgt, unit)
-
-parseColour :: Parser RGB
-parseColour = do
-    void $ PC.char '#'
-    let readHex cs = read ("0x" <> cs)
-    r <- readHex <$> replicateM 2 PC.hexDigitChar
-    g <- readHex <$> replicateM 2 PC.hexDigitChar
-    b <- readHex <$> replicateM 2 PC.hexDigitChar
-    pure $ MkRGB r g b
 
 parseInput :: T.Text -> [M.Map T.Text T.Text]
 parseInput txt =
