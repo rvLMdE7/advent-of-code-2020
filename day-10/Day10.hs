b6888008a63850550dc8bc4de12d9a02f308f04d
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-10 20:52:40 +0000

    do day 10, part 1
    We have some bugfixes, e.g. replace `0` with `1` in `makeHistogram`.
    We also account for duplicate joltages, which it turns out are
    possible, but fortunately this actually doesn't require almost any code
    changes at all.

--- day-10/Day10.hs
+++ day-10/Day10.hs
@@ -4,13 +4,10 @@
 module Day10 where
 
 import Data.ByteString qualified as B
-import Data.Functor (($>))
 import Data.Maybe (fromMaybe)
 import Data.Sequence (Seq((:<|), (:|>)))
 import Data.Sequence qualified as Seq
 import Data.Map qualified as M
-import Data.Set (Set)
-import Data.Set qualified as Set
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
 import Data.Void (Void)
@@ -24,7 +21,7 @@ import Text.Megaparsec.Char.Lexer qualified as PCL
 type Parser = P.Parsec Void T.Text
 
 type Chain = Seq Int
-type Joltages = Set Int
+type Joltages = Seq Int
 
 main :: IO ()
 main = do
@@ -40,12 +37,12 @@ part1 jolts = if validChain chain
     else Left $ [Printf.s|not a valid chain: %?|] chain
   where
     chain = makeChain jolts
-    hist = makeHistogram $ linkLengths chain
+    hist = linkLengths chain
     ones = M.findWithDefault 0 1 hist
     threes = M.findWithDefault 0 3 hist
 
 makeChain :: Joltages -> Chain
-makeChain = Set.toAscList .> Seq.fromList
+makeChain = Seq.sort
 
 validChain :: Chain -> Bool
 validChain = getLinks .> validLinks
@@ -70,27 +67,15 @@ readFileUtf8 :: FilePath -> IO T.Text
 readFileUtf8 path = TE.decodeUtf8 <$> B.readFile path
 
 parseJoltages :: Parser Joltages
-parseJoltages = parseJoltage `sepEndByUniq` PC.newline
+parseJoltages = Seq.fromList <$> P.sepEndBy parseJoltage PC.newline
 
 parseJoltage :: Parser Int
 parseJoltage = PC.space *> PCL.decimal
 
-sepEndByUniq :: (Show a, Ord a) => Parser a -> Parser b -> Parser (Set a)
-sepEndByUniq parser sep = go Set.empty
-  where
-    go acc = P.optional parser >>= \case
-        Nothing -> pure acc
-        Just x -> if x `Set.member` acc
-            then fail $ [Printf.s|repeated value %?|] x
-            else do
-                let newAcc = Set.insert x acc
-                more <- P.option False (sep $> True)
-                if more then go newAcc else pure newAcc
-
 makeHistogram :: (Foldable t, Ord a) => t a -> M.Map a Int
 makeHistogram = foldr insert M.empty
   where
-    insert x = M.insertWith (+) x 0
+    insert x = M.insertWith (+) x 1
 
 between :: Ord a => a -> a -> a -> Bool
 between a b x = (a <= x) && (x <= b)
