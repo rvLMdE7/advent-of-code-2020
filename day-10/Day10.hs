98bde64a596b5684f5b601498e9c462f7319a716
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-10 23:29:27 +0000

    do day 10, part 2

--- day-10/Day10.hs
+++ day-10/Day10.hs
@@ -30,6 +30,7 @@ main = do
         Left err -> putStrLn $ P.errorBundlePretty err
         Right input -> do
             print $ part1 input
+            print $ part2 input
 
 part1 :: Joltages -> Either String Int
 part1 jolts = if validChain chain
@@ -40,6 +41,34 @@ part1 jolts = if validChain chain
     hist = linkLengths chain
     ones = M.findWithDefault 0 1 hist
     threes = M.findWithDefault 0 3 hist
+
+part2 :: Joltages -> Int
+part2 = Seq.sort .> divideIntoSubproblems .> fmap bruteForce .> product
+  where
+    bruteForce (a, xs, b) = bruteForceNumChains a b xs
+
+bruteForceNumChains :: Int -> Int -> Seq Int -> Int
+bruteForceNumChains bot top = bruteForceChains bot top .> Seq.length
+
+bruteForceChains :: Int -> Int -> Seq Int -> Seq (Seq Int)
+bruteForceChains bot top mid = go bot Seq.Empty (mid :|> top)
+  where
+    go :: Int -> Seq Int -> Seq Int -> Seq (Seq Int)
+    go prev out inp = case inp of
+        Seq.Empty -> Seq.singleton (bot :<| out)
+        _ -> do
+            (i, x) <- indexed $ Seq.takeWhileL (diffLE 3 prev) inp
+            go x (out :|> x) (Seq.drop (i + 1) inp)
+
+divideIntoSubproblems :: Seq Int -> Seq (Int, Seq Int, Int)
+divideIntoSubproblems sequ = mapMaybe tryMakeSubproblem subSeqs
+  where
+    bot = 0
+    top = fromMaybe bot (Seq.lookup (Seq.length sequ - 1) sequ) + 3
+    subSeqs = groupByRolling (diffLT 3) (bot :<| (sequ :|> top))
+    tryMakeSubproblem = \case
+        x :<| (mid :|> y) -> Just (x, mid, y)
+        _ -> Nothing
 
 makeChain :: Joltages -> Chain
 makeChain = Seq.sort
@@ -54,8 +83,7 @@ getLinks :: Chain -> Seq (Int, Int)
 getLinks chain = Seq.zip (bot :<| chain) (chain :|> top)
   where
     bot = 0
-    len = Seq.length chain
-    top = fromMaybe bot (Seq.lookup (len - 1) chain) + 3
+    top = 3 + fromMaybe bot (Seq.lookup (Seq.length chain - 1) chain)
 
 linkLengths :: Chain -> M.Map Int Int
 linkLengths chain = makeHistogram lens
@@ -79,3 +107,37 @@ makeHistogram = foldr insert M.empty
 
 between :: Ord a => a -> a -> a -> Bool
 between a b x = (a <= x) && (x <= b)
+
+diffLT :: (Ord a, Num a) => a -> a -> a -> Bool
+diffLT x a b = diff a b < x
+
+diffLE :: (Ord a, Num a) => a -> a -> a -> Bool
+diffLE x a b = diff a b <= x
+
+diff :: Num a => a -> a -> a
+diff a b = abs (b - a)
+
+indexed :: Seq a -> Seq (Int, a)
+indexed s = Seq.fromList [0 .. Seq.length s - 1] `Seq.zip` s
+
+groupByRolling :: (a -> a -> Bool) -> Seq a -> Seq (Seq a)
+groupByRolling comp = go Seq.Empty
+  where
+    go acc = \case
+        x :<| y :<| rest -> if comp x y
+            then go (consBack x acc) (y :<| rest)
+            else go (consBack x acc :|> Seq.Empty) (y :<| rest)
+        x :<| Seq.Empty -> consBack x acc
+        Seq.Empty -> acc
+
+mapMaybe :: (a -> Maybe b) -> Seq a -> Seq b
+mapMaybe f = \case
+    x :<| xs -> case f x of
+        Just res -> res :<| mapMaybe f xs
+        Nothing -> mapMaybe f xs
+    Seq.Empty -> Seq.Empty
+
+consBack :: a -> Seq (Seq a) -> Seq (Seq a)
+consBack x = \case
+    rest :|> xs -> rest :|> (xs :|> x)
+    Seq.Empty -> Seq.singleton (Seq.singleton x)
