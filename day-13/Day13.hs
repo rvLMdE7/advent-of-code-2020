6cff732f469667b256db50680575c8d8299758dd
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-13 13:52:16 +0000

    start day 13, part 2

--- day-13/Day13.hs
+++ day-13/Day13.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Day13 where
 
 import Control.Arrow ((&&&))
@@ -5,7 +7,7 @@ import Control.Monad (void)
 import Data.ByteString qualified as B
 import Data.List qualified as L
 import Data.List.NonEmpty qualified as NE
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, mapMaybe)
 import Data.Ord (comparing)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
@@ -21,15 +23,40 @@ type Parser = P.Parsec Void T.Text
 main :: IO ()
 main = do
     file <- readFileUtf8 "day-13/input.txt"
-    case P.parse (parseInput1 <* P.eof) "day 13 input" file of
+    case P.parse (parseInput1 <* P.eof) "day 13 input, part 1" file of
         Left err -> putStrLn $ P.errorBundlePretty err
-        Right (earliest, buses) -> do
-            print $ part1 earliest buses
+        Right (earliest, buses) -> print $ part1 earliest buses
+    case P.parse (parseInput2 <* P.eof) "day 13 input, part 2" file of
+        Left err -> putStrLn $ P.errorBundlePretty err
+        Right system -> print $ part2 system
 
 part1 :: Int -> NE.NonEmpty Int -> Int
 part1 earliest buses = bus * (time - earliest)
   where
     (bus, time) = minNextMult earliest buses
+
+part2 :: NE.NonEmpty (Integer, Integer) -> Integer
+part2 = solveCoprimes
+
+solveCoprimes :: Integral a => NE.NonEmpty (a, a) -> a
+solveCoprimes = foldr1 reduce .> fst
+  where
+    reduce (a1, n1) (a2, n2) = (solveCoprime (a1, n1) (a2, n2), n1 * n2)
+
+solveCoprime :: Integral a => (a, a) -> (a, a) -> a
+solveCoprime (a1, n1) (a2, n2) = (a1*m2*n2 + a2*m1*n1) `mod` (n1 * n2)
+  where
+    (m1, m2) = bezout n1 n2
+
+bezout :: Integral a => a -> a -> (a, a)
+bezout a b = go a b 1 0
+  where
+    go rOld r sOld s
+        | r == 0    = (sOld, t)
+        | otherwise = go r (rOld - q*r) s (sOld - q*s)
+      where
+        q = rOld `div` r
+        t = if b == 0 then 0 else (rOld - sOld * a) `quot` b
 
 minNextMult :: Int -> NE.NonEmpty Int -> (Int, Int)
 minNextMult modulus =
@@ -49,6 +76,23 @@ parseInput1 = do
         else pure (earliest, NE.fromList busIds)
   where
     busId = P.choice
+        [ Nothing <$ PC.char 'x'
+        , Just <$> PCL.decimal
+        ]
+
+parseInput2 :: forall a. Integral a => Parser (NE.NonEmpty (a, a))
+parseInput2 = do
+    void $ PC.hspace *> (PCL.decimal :: Parser a)
+    void $ PC.hspace *> PC.newline
+    times <- P.sepBy1 time (PC.hspace *> PC.char ',')
+    let maybeIndex may i = (const i &&& id) <$> may
+        list = mapMaybe (uncurry maybeIndex) (zip times [0..])
+    PC.space
+    if null list
+        then fail "not enough inputs"
+        else pure (NE.fromList list)
+  where
+    time = P.choice
         [ Nothing <$ PC.char 'x'
         , Just <$> PCL.decimal
         ]
