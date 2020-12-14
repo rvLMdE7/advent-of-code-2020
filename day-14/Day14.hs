77225b4aa23766f5fdbbf5f551656f7ad5f7476a
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-14 22:38:16 +0000

    do day 14, part 2
    It was quite challenging to refactor things in order to model parts 1
    and 2 into a coherent model. We have changed things so that we delay
    interpreting the bitmask as long as possible, which allows us to reuse
    the same datatype for both parts, and at the last moment we interpret
    it according to which part we are working on.
    
    Having tweaked that around, the challenging part that remains is
    implementing what we have called here `decode2`. In the end we settled
    for whatever did the job, perhaps not the most efficient. Although
    saying that the day still runs in a fraction of a second, so perhaps
    it's not so bad.

--- day-14/Day14.hs
+++ day-14/Day14.hs
@@ -15,11 +15,16 @@ module Day14 where
 import Control.Applicative ((<|>))
 import Control.Monad (void)
 import Control.Monad.State (State, execState)
-import Data.Bits (Bits, (.|.), (.&.), complement, zeroBits)
+import Data.Bifunctor (second)
+import Data.Bits (Bits, (.|.), (.&.), complement, zeroBits, setBit, clearBit)
+import Data.Bool (bool)
 import Data.ByteString qualified as B
+import Data.Foldable (for_)
 import Data.Functor (($>))
 import Data.Int (Int64)
+import Data.List (subsequences)
 import Data.Map qualified as M
+import Data.Set qualified as Set
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
 import Data.Void (Void)
@@ -36,6 +41,105 @@ type Parser = P.Parsec Void T.Text
 
 data Bit = One | Zero | Cross
     deriving (Bounded, Enum, Eq, Ord, Read, Show)
+
+data Instr a
+    = SetMask [Bit]
+    | WriteMem a a
+    deriving (Eq, Ord, Show, Read)
+
+data ProgState1 a = MkProgState1
+    { memory1 :: M.Map a a
+    , bitMask1 :: (a, a)
+    } deriving (Eq, Ord, Show, Read)
+
+makeFieldLabelsWith noPrefixFieldLabels ''ProgState1
+
+data ProgState2 a = MkProgState2
+    { memory2 :: M.Map a a
+    , bitMask2 :: a -> [a]
+    }
+
+makeFieldLabelsWith noPrefixFieldLabels ''ProgState2
+
+main :: IO ()
+main = do
+    file <- readFileUtf8 "day-14/input.txt"
+    case P.parse (parseProgram <* P.eof) "day 14 input" file of
+        Left err -> putStrLn $ P.errorBundlePretty err
+        Right prog -> do
+            print $ part1 prog
+            print $ part2 prog
+
+part1 :: [Instr Int64] -> Int64
+part1 = runProgram1 .> view #memory1 .> sum
+
+part2 :: [Instr Int64] -> Int64
+part2 = runProgram2 .> view #memory2 .> sum
+
+runProgram1 :: (Integral a, Bits a, Ord a) => [Instr a] -> ProgState1 a
+runProgram1 instrs = execState (interpProgram1 instrs) initState
+  where
+    initState = MkProgState1
+        { memory1 = M.empty
+        , bitMask1 = (complement zeroBits, zeroBits)
+        }
+
+interpProgram1
+    :: (Integral a, Bits a, Ord a) => [Instr a] -> State (ProgState1 a) ()
+interpProgram1 = foldr (\i comp -> interpInstr1 i *> comp) (pure ())
+
+interpInstr1
+    :: (Integral a, Bits a, Ord a) => Instr a -> State (ProgState1 a) ()
+interpInstr1 = \case
+    SetMask mask -> #bitMask1 .= decode1 mask
+    WriteMem address value -> do
+        mask <- use #bitMask1
+        #memory1 % at address ?= applyMask mask value
+
+runProgram2 :: (Integral a, Bits a, Ord a) => [Instr a] -> ProgState2 a
+runProgram2 instrs = execState (interpProgram2 instrs) initState
+  where
+    initState = MkProgState2
+        { memory2 = M.empty
+        , bitMask2 = \x -> [x]
+        }
+
+interpProgram2
+    :: (Integral a, Bits a, Ord a) => [Instr a] -> State (ProgState2 a) ()
+interpProgram2 = foldr (\i comp -> interpInstr2 i *> comp) (pure ())
+
+interpInstr2
+    :: (Integral a, Bits a, Ord a) => Instr a -> State (ProgState2 a) ()
+interpInstr2 = \case
+    SetMask mask -> #bitMask2 .= decode2 mask
+    WriteMem rawAddr value -> do
+        mask <- use #bitMask2
+        for_ (mask rawAddr) $ \addr -> #memory2 % at addr ?= value
+
+readFileUtf8 :: FilePath -> IO T.Text
+readFileUtf8 path = TE.decodeUtf8 <$> B.readFile path
+
+applyMask :: Bits a => (a, a) -> a -> a
+applyMask (zeroes, ones) x = (x .&. zeroes) .|. ones
+
+parseProgram :: Integral a => Parser [Instr a]
+parseProgram = parseInstr `P.sepEndBy` PC.newline
+
+parseInstr :: Integral a => Parser (Instr a)
+parseInstr = parseSetMask <|> parseWriteMem
+
+parseSetMask :: Integral a => Parser (Instr a)
+parseSetMask = do
+    void $ PC.hspace *> PC.string "mask"
+    void $ PC.hspace *> PC.char '='
+    rawBits <- PC.hspace *> P.many maskingBit
+    pure $ SetMask rawBits
+  where
+    maskingBit = P.choice
+        [ PC.char '0' $> Zero
+        , PC.char '1' $> One
+        , PC.char 'X' $> Cross
+        ]
 
 -- Observe that, given any mask like
 --
@@ -57,76 +161,33 @@ data Bit = One | Zero | Cross
 -- Given all this, our idea is to represent each mask as two integers,
 -- representing `zeroMask` and `oneMask` as described here.
 
-data Instr a
-    = SetMask a a
-    | WriteMem a a
-    deriving (Eq, Ord, Show, Read)
-
-data ProgState a = MkProgState
-    { memory :: M.Map a a
-    , bitMask :: (a, a)
-    } deriving (Eq, Ord, Show, Read)
-
-makeFieldLabelsWith noPrefixFieldLabels ''ProgState
-
-main :: IO ()
-main = do
-    file <- readFileUtf8 "day-14/input.txt"
-    case P.parse (parseProgram <* P.eof) "day 14 input" file of
-        Left err -> putStrLn $ P.errorBundlePretty err
-        Right prog -> do
-            print $ part1 prog
-
-part1 :: [Instr Int64] -> Int64
-part1 = runProgram .> view #memory .> sum
-
-runProgram :: (Bits a, Ord a) => [Instr a] -> ProgState a
-runProgram instrs = execState (interpProgram instrs) initState
+decode1 :: forall a. Integral a => [Bit] -> (a, a)
+decode1 rawBits = (zeroMask, oneMask)
   where
-    initState = MkProgState
-        { memory = M.empty
-        , bitMask = (complement zeroBits, zeroBits)
-        }
-
-interpProgram :: (Bits a, Ord a) => [Instr a] -> State (ProgState a) ()
-interpProgram = foldr (\i comp -> interpInstr i *> comp) (pure ())
-
-interpInstr :: (Bits a, Ord a) => Instr a -> State (ProgState a) ()
-interpInstr = \case
-    SetMask zeroes ones -> #bitMask .= (zeroes, ones)
-    WriteMem address value -> do
-        mask <- use #bitMask
-        #memory % at address ?= applyMask mask value
-
-readFileUtf8 :: FilePath -> IO T.Text
-readFileUtf8 path = TE.decodeUtf8 <$> B.readFile path
-
-applyMask :: Bits a => (a, a) -> a -> a
-applyMask (zeroes, ones) x = (x .&. zeroes) .|. ones
-
-parseProgram :: Integral a => Parser [Instr a]
-parseProgram = parseInstr `P.sepEndBy` PC.newline
-
-parseInstr :: Integral a => Parser (Instr a)
-parseInstr = parseSetMask <|> parseWriteMem
-
-parseSetMask :: forall a. Integral a => Parser (Instr a)
-parseSetMask = do
-    void $ PC.hspace *> PC.string "mask"
-    void $ PC.hspace *> PC.char '='
-    rawBits <- PC.hspace *> P.many maskingBit
-    let indexed = reverse rawBits `zip` enumFrom (0 :: a)
-        zeroMask = sum (mkZero <$> indexed)
-        oneMask = sum (mkOne <$> indexed)
-    pure $ SetMask zeroMask oneMask
-  where
-    maskingBit = P.choice
-        [ PC.char '0' $> Zero
-        , PC.char '1' $> One
-        , PC.char 'X' $> Cross
-        ]
+    indexed = reverse rawBits `zip` enumFrom (0 :: a)
+    zeroMask = sum (mkZero <$> indexed)
+    oneMask = sum (mkOne <$> indexed)
     mkZero (bits, i) = if bits == Zero then 0 else 2^i
     mkOne (bits, i) = if bits == One then 2^i else 0
+
+decode2 :: forall a. (Integral a, Bits a) => [Bit] -> a -> [a]
+decode2 rawBits x = do
+    floating <- do
+        ixes <- Set.fromList <$> subsequences crossIxes
+        pure $ do
+            ix <- crossIxes
+            pure (ix, ix `Set.member` ixes)
+    pure $ assignBits (ones <> floating) x
+  where
+    bitsIxed = [0 ..] `zip` reverse rawBits
+    crossIxes = fst <$> filter (snd .> (==) Cross) bitsIxed
+    ones = second (const True) <$> filter (snd .> (==) One) bitsIxed
+
+assignBit :: Bits a => Int -> Bool -> a -> a
+assignBit i b x = bool clearBit setBit b x i
+
+assignBits :: Bits a => [(Int, Bool)] -> a -> a
+assignBits list x = foldr (uncurry assignBit) x list
 
 parseWriteMem :: Integral a => Parser (Instr a)
 parseWriteMem = do
