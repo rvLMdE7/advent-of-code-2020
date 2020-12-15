bc17f99d3081e023fec52ae6d1be91e7f4d3d6e8
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-15 21:06:54 +0000

    do day 15, part 2
    This iteration is a lot quicker than previous, but still somewhat slow
    on the whole. It runs part 2 in ~45s on my machine. But I'm not
    immediately sure how to further optimize it, and this is just about
    good enough.
    
    Algorithmically, the approach we take is to keep track of our "memory"
    at each turn. That is, we maintain a map `memory` such that, at any
    given moment, the answer to the question 'on what turn was the number
    `n` last encountered?' is `memory !? n`. And then we do the necessary
    bookkeeping in order to keep this map up-to-date, and simply run the
    game as many turns as needed.
    
    Additionally, in terms of optimizations, we try the obvious things -
    try to avoid as many abstractions as possible in favour of writing
    things out explicitly, unpacking as much as possible, and make strict
    as much as possible.

--- day-15/Day15.hs
+++ day-15/Day15.hs
@@ -1,15 +1,29 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Day15 where
 
 import Control.Monad (replicateM_)
 import Control.Monad.State (State, get, modify, execState)
+import Data.Foldable qualified as Fold
 import Data.Function ((&))
+import Data.IntMap (IntMap)
+import Data.IntMap qualified as IntMap
+import Data.IntSet qualified as IntSet
 import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.List.NonEmpty qualified as List.NE
+import Data.Maybe (fromJust)
 import Data.Sequence qualified as Seq
 import Data.Sequence.NonEmpty (NESeq((:||>)), (|>))
 import Data.Sequence.NonEmpty qualified as Seq.NE
+import Flow ((.>))
 
+
+data GameState = MkGameState
+    { lastTurn :: {-# UNPACK #-} !Int
+    , lastGo :: {-# UNPACK #-} !Int
+    , memory :: !(IntMap Int)
+    } deriving (Eq, Ord, Show, Read)
 
 main :: IO ()
 main = do
@@ -21,16 +35,40 @@ part1 :: NonEmpty Int -> Int
 part1 input = playGame input (2020 - length input) & Seq.NE.last
 
 part2 :: NonEmpty Int -> Int
-part2 input = playGame input (30_000_000 - length input) & Seq.NE.last
+part2 input = playGame' input (30_000_000 - length input) & lastGo
 
 playGame :: NonEmpty Int -> Int -> NESeq Int
 playGame input n = execState (takeTurns n) (Seq.NE.fromList input)
 
+playGame' :: NonEmpty Int -> Int -> GameState
+playGame' input n = takeTurns' n initState
+  where
+    x :| xs = List.NE.reverse input
+    vals = IntSet.fromList $ Fold.toList input
+    lastSeen = (`Seq.NE.elemIndexR` Seq.NE.fromList input) .> fromJust
+    initState = MkGameState
+        { lastGo = x
+        , lastTurn = length xs
+        , memory = IntMap.fromSet lastSeen vals
+        }
+
 takeTurns :: Int -> State (NESeq Int) ()
 takeTurns n = replicateM_ n takeTurn
+
+takeTurns' :: Int -> GameState -> GameState
+takeTurns' n !acc
+    | n == 0 = acc
+    | otherwise = takeTurns' (n - 1) (takeTurn' acc)
 
 takeTurn :: State (NESeq Int) ()
 takeTurn = get >>= \case
     xs :||> x -> do
         let age = maybe 0 (length xs -) (Seq.elemIndexR x xs)
         modify (|> age)
+
+takeTurn' :: GameState -> GameState
+takeTurn' (MkGameState !turn !go !mem) = MkGameState nTurn nGo nMem
+  where
+    nGo = maybe 0 (turn -) (IntMap.lookup go mem)
+    nTurn = turn + 1
+    nMem = IntMap.insert go turn mem
